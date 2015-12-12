27078bce98139d5ce4f801683cdaf90c7cc909b3
Mathieu Desjarlais <github@desmat.ca>
2015-12-12 16:23:00 +0700

    doc

--- components/utils/ClientSideRenderer.jsx
+++ components/utils/ClientSideRenderer.jsx
@@ -8,6 +8,10 @@ var Link = ReactRouter.Link;
 var createBrowserHistory = require('history/lib/createBrowserHistory');
 var routes = require('../Routes.jsx');
 
+/*
+ * On the front-end, look for element react-content and render React component tree
+ */ 
+
 if (typeof document == "object") {
 	//console.log('running router on client side!');
 	var targetElement = document.getElementById('react-content');
