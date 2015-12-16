f7d7aa7433774ece1ddb202d5756c3a951edc7c7
Mathieu Desjarlais <github@desmat.ca>
2015-12-16 21:02:36 +0700

    grunt watch components

--- tasks/config/watch.js
+++ tasks/config/watch.js
@@ -18,10 +18,10 @@ module.exports = function(grunt) {
 		assets: {
 
 			// Assets to watch:
-			files: ['assets/**/*', 'tasks/pipeline.js', '!**/node_modules/**'],
+			files: ['assets/**/*', 'tasks/pipeline.js', 'components/**/*', '!**/node_modules/**'],
 
 			// When assets are changed:
-			tasks: ['syncAssets' , 'linkAssets']
+			tasks: ['syncAssets', 'browserify', 'linkAssets']
 		}
 	});
 
