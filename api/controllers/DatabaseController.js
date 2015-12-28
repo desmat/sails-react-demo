a331345ba77955803f4b82356ec4eb3d12c5f320
Mathieu Desjarlais <github@desmat.ca>
2015-12-28 11:50:56 -0500

    added database controller for easy debug (should remove at the end)

---
+++ api/controllers/DatabaseController.js
@@ -0,0 +1,11 @@
+/**
+ * DatabaseController
+ *
+ * @description :: Server-side logic for managing databases
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
