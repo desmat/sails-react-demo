d7d5bda9bae186d55952752ee8b21db88331a1c0
Mathieu Desjarlais <github@desmat.ca>
2015-12-28 16:04:13 -0500

    isomorphic supports queries in data model

--- api/controllers/AboutController.js
+++ api/controllers/AboutController.js
@@ -7,14 +7,5 @@
 
 module.exports = {
 	
-
-  database: function (req, res) {
-    Database.find({}, function(err, rs) {
-      if (err) return console.log(err);
-      console.dir(rs);
-    });
-
-    res.send('ok');
-  }
 };
 
