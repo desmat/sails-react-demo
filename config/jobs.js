6916e61a893a98ec64341b65984c3c3d36857aae
Mathieu Desjarlais <github@desmat.ca>
2015-12-29 13:06:04 -0500

    jobs

---
+++ config/jobs.js
@@ -0,0 +1,20 @@
+/**
+ * Jobs to be initialized at startup
+ *
+ */
+module.exports.jobs = [
+  // {
+  //   name: "Job 1", 
+  //   interval: 500, 
+  //   job: function() {
+  //     console.log('*** Job 1 ***');
+  //   }
+  // },
+  // {
+  //   name: "Job 2", 
+  //   interval: 800, 
+  //   job: function() {
+  //     console.log('*** Job 2 ***');
+  //   }
+  // },
+]
