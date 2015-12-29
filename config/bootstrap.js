f684e96bfd41499a9e5e6f24f886fe9b57a11d3f
Mathieu Desjarlais <github@desmat.ca>
2015-12-29 12:00:42 -0500

    initial data

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -11,7 +11,39 @@
 
 module.exports.bootstrap = function(cb) {
   // Load up initial data
-  require('./data').load();
+  //require('./data').load();
+
+  //look at database table, compare with data structure above, insert missing records, mark as inserted in database  
+  var data = require('./data').data;
+
+  Database.count().then(function(count) {
+    //console.log('database count: ' + count);
+    //console.log('data count: ' + data.length);
+
+    if (count < data.length) {
+      _.each(data.slice(count, data.length), function(data) {
+        
+        //split record as name-value pair
+        _.each(data, function(k, v) {          
+
+          sails.log.debug('Loading up intial [' + v + '] records');          
+          this[v].create(k, function(err, created) {
+            if (err) {
+              sails.log.err(err);
+            }
+            else {
+              //mark as inserted
+              Database.create({model: v, count: (typeof k === 'Array' ? k.length : 1)}, function() {
+                if (err) sails.log.err(err);
+                //else sails.log.debug('marked [' + v + '] records as inserted');          
+              });
+            }
+          });
+        });
+      });
+    }
+  });
+
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
--- config/data.js
+++ config/data.js
@@ -1,5 +1,24 @@
-
-var data = [
+/**
+ * Initial data to load in api models.
+ *
+ * Expected is an array of records in the form:
+ *  [
+ *    ModelName: [
+ *      {
+ *        attributeName: "value", 
+ *        otherAttribute: "otherValue"
+ *      },
+ *      {
+ *        attributeName: "yet another", 
+ *        otherAttribute: "and so forth"
+ *      },
+ *    ], 
+ *    OtherModel: [
+ *      ...
+ *    ]
+  * ]
+ */
+module.exports.data = [
   {
     Todo: [
       {
@@ -24,41 +43,8 @@ var data = [
       }
     ]
   }, 
-  // Additional records
-  // {
-  //   About: {text: 'More records...'}
-  // }
+  //Additional records
+  {
+    About: {text: 'More records...'}
+  }
 ];
-
-module.exports.load = function() {
-  
-  //look at database table, compare with data structure above, insert missing records, mark as inserted in database  
-  
-  Database.count().then(function(count) {
-    //console.log('database count: ' + count);
-    //console.log('data count: ' + data.length);
-
-    if (count < data.length) {
-      _.each(data.slice(count, data.length), function(data) {
-        
-        //split record as name-value pair
-        _.each(data, function(k, v) {          
-
-          sails.log.debug('Loading up intial [' + v + '] records');          
-          this[v].create(k, function(err, created) {
-            if (err) {
-              sails.log.err(err);
-            }
-            else {
-              //mark as inserted
-              Database.create({model: v, count: (typeof k === 'Array' ? k.length : 1)}, function() {
-                if (err) sails.log.err(err);
-                //else sails.log.debug('marked [' + v + '] records as inserted');          
-              });
-            }
-          });
-        });
-      });
-    }
-  });
-};
