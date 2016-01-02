d37947ca064ed1e9640e8143b93fe190706d1b01
Mathieu Desjarlais <github@desmat.ca>
2016-01-02 11:35:24 -0500

    cleaned up login

---
+++ api/controllers/ApiController.js
@@ -0,0 +1,11 @@
+/**
+ * ApiController: overwrite default api end-points
+ *
+ * @description :: Server-side logic for managing apis
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
